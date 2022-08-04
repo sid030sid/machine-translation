3f6be0b2e78c9a67441a55d39c540589b81360a7
sidlami <72751704+sidlami@users.noreply.github.com>
2022-08-04 11:59:25 +0200

    documentation of performance measuring experiment

--- final.py
+++ final.py
@@ -7,6 +7,7 @@ from keras.layers import Input, RepeatVector, GRU, Dense, TimeDistributed, Embed
 from keras.preprocessing.text import Tokenizer
 from keras_preprocessing.sequence import pad_sequences
 from keras.initializers import Constant
+import dataframe_image as dfi
 
 # import corpuss
 eng = pd.read_csv("data/europarl-v7.nl-en.en", sep="\t", names=["eng"])
@@ -14,17 +15,38 @@ nl = pd.read_csv("data/europarl-v7.nl-en.nl", sep="\t", names=["nl"], on_bad_lin
 corpus = pd.DataFrame(eng)
 corpus["nl"] = nl
 
-# select random data of corpus for tasks 2-4
-## note: due to limited resources, 3% of the short sentences (<= 20 words) are taken as a sample
-#idx_short_sentences = [idx for idx, sentence in enumerate(corpus.nl) if len(sentence.split(" ")) <= 20] # the short sentence are determined by the dutch corpus as dutch tends to use more words than the english equivalent
-#corpus_short = corpus.filter(items=idx_short_sentences, axis=0)
-#sample = corpus_short.sample(round(len(corpus_short) * 0.03))
-sample = corpus.sample(round(len(corpus) * 0.01))
+# TASK 1: analysis of corpus
+# number of words
+words_eng = [word for sentence in corpus.eng for word in sentence.split()]
+words_nl = [word for sentence in corpus.nl for word in sentence.split()]
 
+# compare length of same sentence in english and in dutch
+num_word_per_sentence_eng = [len(sentence.split(" ")) for sentence in corpus.eng]
+num_word_per_sentence_nl = [len(sentence.split(" ")) for sentence in corpus.nl]
+
+summary_corpus = pd.DataFrame({
+    "Language":["English", "Dutch"], 
+    "Number of sentences" : [len(corpus.eng), len(corpus.nl)],
+    "Number of words" : [len(words_eng), len(words_nl)],
+    "Minimum number of words per sentence" : [min(num_word_per_sentence_eng), min(num_word_per_sentence_nl)],
+    "Average number of words per sentence" : [np.mean(num_word_per_sentence_eng), np.mean(num_word_per_sentence_nl)],
+    "Maximum number of words per sentence" : [max(num_word_per_sentence_eng), max(num_word_per_sentence_nl)],
+}) 
+dfi.export(summary_corpus, "documentation/tables_as_image/summary_corpus.png")
+
+# select random data of corpus for tasks 2-4
+## note: due to limited resources, 3% of the short sentences (<= 50 words) are taken as a sample
+nl_idx_short_sentences = [idx for idx, sentence in enumerate(corpus.nl) if len(sentence.split(" ")) <= 50] # the short sentence are determined by the dutch corpus as dutch tends to use more words than the english equivalent
+eng_idx_short_sentences = [idx for idx, sentence in enumerate(corpus.eng) if len(sentence.split(" ")) <= 50]
+idx_short_sentences = list(set(nl_idx_short_sentences) & set(eng_idx_short_sentences))
+corpus_short = corpus.filter(items=idx_short_sentences, axis=0)
+sample = corpus_short.sample(round(len(corpus_short) * 0.03))
+
+# transform english and dutch corpus into single numpy arrays for frther steps
 eng_sentences = np.array(sample.eng)
 nl_sentences = np.array(sample.nl)
 
-# pre-pprocessing (task 2)
+# TASK 2: pre-pprocessing
 def clean_sentence(sentence):
     # Lower case the sentence
     lower_case_sent = sentence.lower()
@@ -52,21 +74,7 @@ eng_text_tokenized_word, eng_text_tokenizer_word = tokenize(eng_sentences_clean)
 nl_text_tokenized_char, nl_text_tokenizer_char = tokenize(nl_sentences_clean, character_based=True)
 eng_text_tokenized_char, eng_text_tokenizer_char = tokenize(eng_sentences_clean, character_based=True)
 
-# Analysis of sample
-print('Maximum length dutch sentence: {}'.format(len(max(nl_text_tokenized_word,key=len))))
-print('Maximum length english sentence: {}'.format(len(max(eng_text_tokenized_word,key=len))))
-
-# Check language length
-nl_vocab = len(nl_text_tokenizer_word.word_index) + 1
-eng_vocab = len(eng_text_tokenizer_word.word_index) + 1
-nl_char = len(eng_text_tokenizer_char.word_index) + 1
-eng_char = len(eng_text_tokenizer_char.word_index) + 1
-print("Dutch vocabulary is of {} unique words".format(nl_vocab))
-print("English vocabulary is of {} unique words".format(eng_vocab))
-print("Dutch vocabulary is of {} characters".format(nl_char))
-print("English vocabulary is of {} characters".format(eng_char))
-
-# Respective max sentence length
+# respective max sentence length in words or characters: crucial for padding and building all upcoming models
 max_nl_len_word = int(len(max(nl_text_tokenized_word,key=len)))
 max_eng_len_word = int(len(max(eng_text_tokenized_word,key=len)))
 max_nl_len_char = int(len(max(nl_text_tokenized_char,key=len)))
@@ -84,8 +92,38 @@ eng_pad_sentence_word = eng_pad_sentence_word.reshape(*eng_pad_sentence_word.sha
 nl_pad_sentence_char = nl_pad_sentence_char.reshape(*nl_pad_sentence_char.shape, 1)
 eng_pad_sentence_char = eng_pad_sentence_char.reshape(*eng_pad_sentence_char.shape, 1)
 
+# amount of unique words: crucial for building all upcoming models
+nl_vocab = len(nl_text_tokenizer_word.word_index) + 1
+eng_vocab = len(eng_text_tokenizer_word.word_index) + 1
+nl_char = len(eng_text_tokenizer_char.word_index) + 1
+eng_char = len(eng_text_tokenizer_char.word_index) + 1
+
+# summary of sample for documentation purposes
+eng_avg_len = np.mean([len(sentence) for sentence in eng_text_tokenized_word]) # average amount of words in a sentence of englisch corpus
+eng_num_word = sum([len(sentence) for sentence in eng_text_tokenized_word]) # number of words in english corpus
+eng_max_len = len(max(eng_text_tokenized_word,key=len))
+eng_min_len = len(min(eng_text_tokenized_word,key=len))
+
+nl_avg_len = np.mean([len(sentence) for sentence in nl_text_tokenized_word]) # average amount of words in a sentence of dutch corpus
+nl_num_word = len([len(sentence) for sentence in eng_text_tokenized_word]) # number of words in dutch corpus
+nl_max_len = len(max(nl_text_tokenized_word,key=len))
+nl_min_len = len(min(nl_text_tokenized_word,key=len))
+
+summary_sample = pd.DataFrame({
+    "Language":["English", "Dutch"], 
+    "Number of sentences" : [len(sample.eng), len(sample.nl)],
+    "Minimum number of words per sentence" : [eng_min_len, nl_min_len],
+    "Average number of words per sentence" : [eng_avg_len, nl_avg_len],
+    "Maximum number of words per sentence" : [eng_max_len, nl_max_len],
+    "Number of words" : [eng_num_word, nl_num_word],
+    "Number of words (no duplicates)" : [eng_vocab, nl_vocab], 
+    "Nmber of used characters (no duplicates)" : [eng_char, nl_char],
+})
+dfi.export(summary_sample, "documentation/tables_as_image/summary_sample.png")
 
 
+
+# TASK 3: comparison of performance of neural machine translator depending word embedding and tokenization (word or character)
 # function for building ecoder-decoder model without embedding
 def encdec_model(input_shape, output_max_len, output_vocab_size):
     model = Sequential()
@@ -116,12 +154,7 @@ def keras_embd_encdec_model(input_shape, output_max_len, output_vocab_size):
     model.add(TimeDistributed(Dense(output_vocab_size, activation = 'softmax')))
     model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
-eng_pad_sentence_word.shape
-nl_pad_sentence_word.shape
-max_nl_len_word
-max_eng_len_word
-eng_vocab
-nl_vocab
+
 # build word-based english to dutch and dutch to english translator with keras embedding
 eng2nl_keras_embd_encdec_word_model = keras_embd_encdec_model(eng_pad_sentence_word.shape, max_nl_len_word, nl_vocab)
 nl2eng_keras_embd_encdec_word_model = keras_embd_encdec_model(nl_pad_sentence_word.shape, max_eng_len_word, eng_vocab)
@@ -166,8 +199,7 @@ def glove_embd_encdec_model(input_shape, output_max_len, output_vocab_size):
 eng2nl_glove_embd_encdec_word_model = glove_embd_encdec_model(eng_pad_sentence_word.shape, max_nl_len_word, nl_vocab)
 
 #other pretrained weights: https://code.google.com/archive/p/word2vec/
-
-model_training_manuals = [
+done_models = [
     {   
         "title" : "English to Dutch translator (word-based)",
         "model" : eng2nl_encdec_word_model,
@@ -192,12 +224,9 @@ model_training_manuals = [
         "X" : nl_pad_sentence_char,
         "y" : eng_pad_sentence_char
     },
-    {   
-        "title" : "English to Dutch translator (word-based, Glove embedding)",
-        "model" : eng2nl_glove_embd_encdec_word_model,
-        "X" : eng_pad_sentence_word,
-        "y" : nl_pad_sentence_word
-    },
+]
+model_training_manuals = [
+    
     {   
         "title" : "English to Dutch translator (word-based, Keras' embedding)",
         "model" : eng2nl_keras_embd_encdec_word_model,
@@ -221,6 +250,12 @@ model_training_manuals = [
         "model" : nl2eng_keras_embd_encdec_char_model,
         "X" : nl_pad_sentence_char,
         "y" : eng_pad_sentence_char
+    },
+    {   
+        "title" : "English to Dutch translator (word-based, Glove embedding)",
+        "model" : eng2nl_glove_embd_encdec_word_model,
+        "X" : eng_pad_sentence_word,
+        "y" : nl_pad_sentence_word
     },
 ]
 
