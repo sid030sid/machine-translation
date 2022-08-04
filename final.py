f6c5f43109743bf540a02774498c1ef11f68858c
sidlami <72751704+sidlami@users.noreply.github.com>
2022-08-04 19:02:49 +0200

    road map for task 4

--- final.py
+++ final.py
@@ -237,8 +237,12 @@ eng2nl_word2vec_embd_encdec_word_model = word2vec_embd_encdec_model(eng_pad_sent
 
 # TASK 4: neural machine translation with attention (based on guide: https://towardsdatascience.com/light-on-math-ml-attention-with-keras-dc8dbc1fad39)
 #https://github.com/thushv89/attention_keras
+
+https://pypi.org/project/keras-self-attention/
+
 from attention_keras.src.layers.attention import AttentionLayer
 
+# try to use kears' built in attention layer: https://www.google.com/search?q=how+to+use+keras%27+attention+layer&rlz=1C1CHBF_deDE912DE912&oq=how+to+use+keras%27+attention+layer&aqs=chrome..69i57j0i22i30j0i390l5.768642j0j9&sourceid=chrome&ie=UTF-8
 def attention_encdec_model():
     model = Sequential()
     return model
